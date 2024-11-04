cbc737ebf6b687f84c4b36334185662dd3907a75
Lambda <61808223+L-mbda@users.noreply.github.com>
2024-11-04 04:59:02 +0000

    Commit changes

--- src/platform/Account.ts
+++ src/platform/Account.ts
@@ -5,6 +5,8 @@
 import {db} from '@/db/db'
 import { user } from "@/db/schema";
 import { redirect } from 'next/navigation';
+import * as crypto from 'crypto';
+import { eq } from 'drizzle-orm';
 
 /*
     Our create owner account function allows us to create an account
@@ -18,19 +20,55 @@ export async function createOwnerAccount(formInfo: FormData) {
     let [fullName, accountUsername, password] = [
         formResponse.fullName,
         formResponse.username,
-        formResponse.password,
+        // Create a hash
+        await crypto.createHash("sha3-256").update(formResponse.password + "").digest("hex"),
     ]
+    // Create the salting variables
+    var salt1 = crypto.randomBytes(256).toString('hex');
+    var salt2 = crypto.randomBytes(256).toString('hex');
     // Create a check to see if there are any users, if not, then make the user and give them owner.
     if ((await (await db).select().from(user)).length == 0) {
-        (await db).insert(user).values({
+        await (await db).insert(user).values({
             // Need to ignore such errors, occur frequently
             // @ts-ignore
             name: fullName,
             username: accountUsername,
-            password: password,
+            salt1: salt1,
+            salt2: salt2,
+            // Hash the password utilizing SHA512
+            password: await crypto.createHash("sha3-512").update(salt1 + password + salt2).digest("hex"),
             role: "owner"
         })
         // Redirect back to theta to allow for login
         return redirect('/theta')
     }
+}
+
+/*
+    This function lets you log into the application as a server
+    action.
+*/
+export async function loginToAccount(data: FormData) {
+    // get data
+    let formResponse = (Object.fromEntries(await data));
+    // Create array defining all the variables
+    let [username, password] = [formResponse.username,
+        // Ignore the error obtained from the form response password
+        // @ts-ignore
+        await crypto.createHash("sha3-256").update(formResponse.password + "").digest("hex"),
+    ]
+    // Check to see if the user is equal, if not, then do some stuff. Keep ignoring
+    // for some linting reason
+    // @ts-ignore
+    let check = (await (await db).select().from(user).where(eq(user.username, username)));
+    // If the check is not equal to 0, move on and continue
+    if (check.length != 0) {
+        // Salt the password so it'll be more secure.
+        let saltedPassword = await crypto.createHash("sha3-512").update(check[0].salt1 + password + check[0].salt2).digest("hex");
+        // Get first instance and check if the password is equal
+        if (check[0].password == saltedPassword) {
+            console.log("EQUAL!")
+        }
+    }
+    console.log(formResponse);
 }
---
+++ src/platform/components/SSO.tsx
@@ -0,0 +1,12 @@
+// Use Client
+'use client';
+
+// Theta SSO Provider
+export function SSOProvider({className}: any) {
+    return (
+        <div className={className + "gap-4 flex flex-col"}>
+            <hr />
+            <p>Or Sign In With</p>
+        </div>
+    )
+}
